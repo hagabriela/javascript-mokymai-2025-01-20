0163120102bd946fe44c8043a3dfbfdcc6d149d6
Gabriela Hartwig <hartwig.gabriela@yahoo.com>
2025-03-06 12:05:33 +0200

    js dom, selektoriai, antra dalis

--- 16-js-dom/01-pasiemimai/scripts.js
+++ 16-js-dom/01-pasiemimai/scripts.js
@@ -28,6 +28,42 @@ console.log(forma);
 console.log('vardo input:' , forma.children.vardas);
 console.log('el. pašto input:' , forma.children.elpastas);
 
+// darbas su prekėmis
+
+// gauti visas prekes iš suvesto html
+
+let prekes = document.querySelectorAll('.preke')
+console.log('Surastos prekės:' , prekes);
+
+// gauti visą pirmos prekės bloką
+
+let pirmaPreke = document.querySelector('.preke1')
+// let pirmaPreke = document.getElementsByClassName('preke1')
+console.log('Pirmos prekės blokas:' , pirmaPreke);
+
+// gauti visą antros prekės bloką
+
+let antraPreke = document.querySelector('.preke2')
+// let antraPreke = document.getElementsByClassName('preke2')
+console.log('Antros prekės blokas:' , antraPreke);
+
+// gauti pirmos prekės pavadinimą (žymą kurioje yra pavadinimas)
+
+let prekes1Pavadinimas = document.querySelector('.preke1 h3')
+console.log('Pirmos prekės pavadinimas:' , prekes1Pavadinimas);
+
+let pavadinimasV2 = pirmaPreke.querySelector('h3')
+console.log('Pirmos prekės pavadinimas:' , pavadinimasV2);
+
+// gauti antros prekės kainą (žymą kurioje yra kaina)
+
+let prekes2Kaina = antraPreke.querySelector('.kaina')
+console.log('Antros prekės kaina:' , prekes2Kaina);
+
+
+
+
+
 
 
 
