c16b59a651b88de061d2254f5dff61264f1ab4f4
Gabriela Hartwig <hartwig.gabriela@yahoo.com>
2025-03-07 10:20:29 +0200

    js dom interakcijos, didinti-po-10 pavyzdys

--- 16-js-dom/03-interakcijos/scripts.js
+++ 16-js-dom/03-interakcijos/scripts.js
@@ -13,6 +13,22 @@ skaiciuDidinimasMygtukas.addEventListener('click' , function() {
     skaicius++
     // console.log(typeof skaicius, skaicius);
     skaiciuDidinimasMygtukas.textContent = skaicius
-    
+})
 
+// ================================
+// skaiciu-didinimas zona
+// ================================
+
+let didintiPo10Mygtukas = document.querySelector('.didinti-po-10 button')
+// console.log(didintiPo10Mygtukas);
+
+didintiPo10Mygtukas.addEventListener('click' , function() {
+    // alert('paspaudei!')
+    let skaicius = parseInt(didintiPo10Mygtukas.textContent)
+    // let skaicius = parseInt(didintiPo10Mygtukas.textContent)+ 10
+    skaicius += 10
+    // skaicius = skaicius + 10
+    // console.log(typeof skaicius, skaicius);
+    didintiPo10Mygtukas.textContent = skaicius
+    // didintiPo10Mygtukas.textContent = skaicius + 10
 })
