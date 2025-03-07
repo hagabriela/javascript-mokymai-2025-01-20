a1c31ff96ad45b82b840deaba99a83b7821890aa
Gabriela Hartwig <hartwig.gabriela@yahoo.com>
2025-03-07 13:34:16 +0200

    js dom interakcijos, įvairūs skaičiaus keitimai

--- 16-js-dom/03-interakcijos/scripts.js
+++ 16-js-dom/03-interakcijos/scripts.js
@@ -39,7 +39,7 @@ didintiPo10Mygtukas.addEventListener('click' , function() {
 
 let didintiMazintiAts = document.querySelector('.didinti-mazinti p')
 // console.log(didintiMazintiAts);
-// susikurem auksciau, nes idejus i vidu (eventListenerio) nematytu-nepaimtu
+// susikurem kintamaji auksciau, nes idejus i vidu (eventListenerio) nematytu-nepaimtu
 
 let didintiMazintiSk = 0
 let didintiMazintiLimitas = 8
@@ -59,4 +59,48 @@ document.querySelector('.didinti-mazinti .didinti').addEventListener('click' , (
         didintiMazintiSk++
         didintiMazintiAts.textContent = didintiMazintiSk
     }
+})
+
+// ================================
+// ivairus-sk-keitimai zona
+// ================================
+
+let skKeitimaiAts = document.querySelector('.ivairus-sk-keitimai .atsakymas p span')
+// console.log(skKeitimaiAts);
+
+let keiciamasSk = 0
+// console.log(typeof keiciamasSk, keiciamasSk);
+
+
+document.querySelector('.ivairus-sk-keitimai .daugiau-vienu').addEventListener('click' , () => {
+    // console.log('daugiau vienu');
+        keiciamasSk++
+        // keiciamasSk += 1
+        skKeitimaiAts.textContent =  keiciamasSk
+})
+
+document.querySelector('.ivairus-sk-keitimai .daugiau-penkiais').addEventListener('click' , () => {
+    // console.log('daugiau penkiais');
+    keiciamasSk += 5  
+    skKeitimaiAts.textContent =  keiciamasSk
+})
+
+document.querySelector('.ivairus-sk-keitimai .daugiau-desimt').addEventListener('click' , () => {
+    // console.log('daugiau desimt');
+    keiciamasSk += 10  
+    skKeitimaiAts.textContent =  keiciamasSk
+})
+
+document.querySelector('.ivairus-sk-keitimai .maziau-vienu').addEventListener('click' , () => {
+    // console.log('maziau vienu');
+    if (keiciamasSk > 0) {
+        keiciamasSk--
+        skKeitimaiAts.textContent =  keiciamasSk
+    }
+})
+
+document.querySelector('.ivairus-sk-keitimai .grazinti-nuli').addEventListener('click' , () => {
+    // console.log('grazinti nuli');
+    keiciamasSk = 0
+    skKeitimaiAts.textContent =  keiciamasSk
 })
