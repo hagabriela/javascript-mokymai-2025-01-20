58eedfff2f6eec56a8bdd58289a01bb14603d725
Gabriela Hartwig <hartwig.gabriela@yahoo.com>
2025-03-10 11:36:02 +0200

    js dom, elementų togglinimas

---
+++ 16-js-dom/04-stiliai/scripts.js
@@ -0,0 +1,30 @@
+// alert('labas')
+
+// ==================================
+// perjungti blokas
+// ==================================
+
+document.querySelector('.perjungti button').addEventListener('click' , (event) => {
+    // console.log(event); // surašo visą info apie tai kas kątik įvyko (apie patį paspaudimą)
+    // console.log(event.target.textContent); // event.target - yra mygtukas ant kurio kątik spaudėm ir imam jo tekstą
+    // console.log('paspausta');
+    let turinys = document.querySelector('.perjungti .turinys')
+    // console.log(turinys);
+    // console.dir(turinys);
+
+    if (event.target.textContent == 'Įjungti') {
+        // console.log('reikia įjungti');
+        event.target.textContent = 'Išjungti'
+        turinys.style.display = 'block'
+    } else {
+        // console.log('reikia išjungti');
+        event.target.textContent = 'Įjungti'
+        turinys.style.display = 'none'
+    }
+})
+
+// let mygtukas = document.querySelector('.perjungti button')
+
+// mygtukas.addEventListener('click' , () => {
+//     console.log(mygtukas);
+// })
