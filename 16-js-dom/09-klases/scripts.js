7e104b57ad67eec7cfeb9d64d348858201098de3
Gabriela Hartwig <hartwig.gabriela@yahoo.com>
2025-03-12 10:26:31 +0200

    js dom modal pavyzdys

--- 16-js-dom/09-klases/scripts.js
+++ 16-js-dom/09-klases/scripts.js
@@ -16,3 +16,27 @@ document.querySelector('.junginejimai .nuimti-viska').addEventListener('click' ,
     document.querySelector('.junginejimai p').classList.remove('ryskus' , 'spalvinti')
 })
 // jei nera tos klases tuo metu, nesugriauna
+
+// ==============================
+// modalo-ijungimas dalis
+// ==============================
+
+let modalas = document.querySelector('.modal-wrapper')
+
+document.querySelector('.modalo-ijungimas button').addEventListener('click' , () => {
+    modalas.classList.toggle('active')
+    // modalas.classList.add('active')
+})
+
+
+document.querySelector('.modal-wrapper .backdrop').addEventListener('click' , () => {
+    modalas.classList.toggle('active')
+    // modalas.classList.remove('active')
+})
+
+document.querySelector('.modal-wrapper .modal button').addEventListener('click' , () => {
+    modalas.classList.toggle('active')
+    // modalas.classList.remove('active')
+})
+// galima buvo ir su add bei remove
+// pats toggle veikia kaip add ir remove, bet pats atsirenka kada ka
