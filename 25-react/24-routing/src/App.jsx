b0048febf97887851f8200fa6fe68ed209969f97
Gabriela Hartwig <hartwig.gabriela@yahoo.com>
2025-04-08 14:31:21 +0300

    react routing papildyta

--- 25-react/24-routing/src/App.jsx
+++ 25-react/24-routing/src/App.jsx
@@ -9,6 +9,8 @@ import AboutUsPage from './pages/AboutUsPage';
 import ServicesPage from './pages/ServicesPage';
 import NotFoundPage from './pages/NotFoundPage';
 
+// Čia ir aprašomos taisyklės
+
 function App() {
 
   return (
@@ -16,11 +18,24 @@ function App() {
       <Routes>
         {/* Svarbu eiliškumas, kaip if - kurį randa, tą ir vykdo */}
         <Route path='/' element={<MainLayout />}>
+          {/* homepage: /  */}
           <Route path='/' element={<HomePage />} />
+          {/* about us: /about-us  */}
           <Route path='/about-us' element={<AboutUsPage />} />
+          {/* <Route path='/about-us/id:' element={<AboutUsPage />} /> */}
+          {/* services: /services  */}
           <Route path='/services' element={<ServicesPage />} />
-          <Route path='*' element={<NotFoundPage />} /> {/* Kaip else dalis */}
+          {/* visi kiti  */}
+          <Route path='*' element={<NotFoundPage />} /> {/* Kaip else dalis - visi kiti psl. */}
         </Route>
+        {/* <Route path='/admin' element={<AdminLayout />}>
+           admin pradinis: /admin
+          <Route path='/' element={<DashBoardPage />} />
+          statistika: /admin/statistics
+          <Route path='/statistics' element={<StatisticsPage />} />
+          užsakymų puslapis: /admin/orders
+          <Route path='/admin/orders' element={<OrdersPage />} />
+        </Route> */}
       </Routes>
     </BrowserRouter>
   )
